efc4ac6ab746d3da0c9ef24798d0f8343fcb0c37
Mary Wangui <107637548+Zeegaths@users.noreply.github.com>
2024-09-05 00:29:04 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold"> Next TS starter</h1>
+      </div>
+    </section>
+ 
+  )
+}
